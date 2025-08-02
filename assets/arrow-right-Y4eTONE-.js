b97f44b098efc4f06cbf05fe9cc08febe4d477f8
james <james@karma.today>
2025-08-02 16:09:32 +0900

    Updates

---
+++ assets/arrow-right-Y4eTONE-.js
@@ -0,0 +1,8 @@
+import{m as l}from"./core-D2nAxryv.js";import"./index-Div2aLY_.js";import"./index.es-y1U2iwvi.js";const i=l`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M1 7.99a1 1 0 0 1 1-1h7.58L7.12 4.53A1 1 0 1 1 8.54 3.1l4.16 4.17a1 1 0 0 1 0 1.41l-4.16 4.17a1 1 0 1 1-1.42-1.41l2.46-2.46H2a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{i as arrowRightSvg};
